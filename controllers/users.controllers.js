3b07e5f98206350990d892e73225a3e620a2a50f
Jose Camacho <jcamachor1@est.ups.edu.ec>
2023-07-07 13:14:41 -0500

    Add CreateUser

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -18,7 +18,7 @@ usersController.getUsersAll = async (req, res) => {
 usersController.getUserId = async (req, res) => {
     try {
       const sql = "SELECT * FROM usuarios WHERE ID_usuario = ?";
-      const id = req.params.id;
+      const id = req.body.id;
       con.query(sql, [id], function(err, result) {
           console.log(result);
           res.send(result);
@@ -28,5 +28,33 @@ usersController.getUserId = async (req, res) => {
       res.status(500).send("Error al obtener el usuario.");
     }
   };  
+
+  usersController.CreateUser = async (req, res) => {
+    try {
+      const sql = "INSERT INTO usuarios (ID_usuario,nombre,apellido,usuario,password,fecha_inicio_sesion) VALUES (0,?,?,?,?,?)"; 
+      const values = [req.body.nombre,req.body.apellido,req.body.usuario,req.body.password,req.body.fecha_inicio_sesion];
+      con.query(sql, values, function(err, result) {
+        console.log(values);
+        console.log(result);
+        res.send(result);
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("Error al crear el Post.");
+    }
+  };
+
+  usersController.DeleteUserId = async (req, res) => {
+    try {
+      const sql = "DELETE FROM usuarios WHERE ID_usuario = ?";
+      const id = req.body.id;
+      con.query(sql, [id], function(err, result) {
+          console.log("Usuario eliminado");
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("Usuario no eliminado!");
+    }
+  }
 
 module.exports = usersController;
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -3,10 +3,11 @@ const router=Router();
 
 users=require('../controllers/users.controllers');
 
-router.get('/', users.getUsersAll);
-//router.post('/users', );
+router.get('/users', users.getUsersAll);
+router.get('/:id', users.getUserId);
+router.post('/users', users.CreateUser);
 //router.put('', );
-//router.delete('',);
+router.delete('/:id', users.DeleteUserId);
 
 
 module.exports = router;
