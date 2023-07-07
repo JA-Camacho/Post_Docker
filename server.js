f51a5809d1395d098c509928a78d3e502e2b0c8e
Jose Camacho <jcamachor1@est.ups.edu.ec>
2023-07-06 23:49:39 -0500

    Add middlewares and routes

--- server.js
+++ server.js
@@ -5,9 +5,12 @@ const app = express();
 app.set('puerto', process.env.PORT || 8080);
 app.set('nombreApp', 'Aplicacion para Posts con docker');
 
-app.get('/', (req, res) => {
-    res.send('¡Hola, mundo!');
-  });
+//Middlewares
+app.use(express.json());
+
+//Routes
+app.use('/api/user', require('./routes/users.routes'));
+app.use('/api/post', require('./routes/posts.routes'));
 
 //Starting the server
 app.listen(app.get('puerto'), () =>{
