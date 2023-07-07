cf772b16740fcef3c1b4eba007294d469f9d632a
Jose Camacho <jcamachor1@est.ups.edu.ec>
2023-07-06 23:48:55 -0500

    Add auto increment

--- User_table.sql
+++ User_table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE ContentHub.usuarios (
-  ID_usuario INT PRIMARY KEY,
+  ID_usuario INT AUTO_INCREMENT PRIMARY KEY,
   nombre VARCHAR(50),
   apellido VARCHAR(50),
   usuario VARCHAR(50),
