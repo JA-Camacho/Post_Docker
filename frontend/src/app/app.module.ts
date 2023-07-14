8e0152d1a1c351f543838686947d05c1f3edf538
Jose Camacho <jcamachor1@est.ups.edu.ec>
2023-07-14 12:42:03 -0500

    Design footer

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NewPostComponent } from './new-post/new-post.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NewPostComponent
+    NewPostComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
