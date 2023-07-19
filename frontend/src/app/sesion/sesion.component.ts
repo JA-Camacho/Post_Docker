e262da6d4961a23b73597faae3f496e4ab39d440
Jose Camacho <jcamachor1@est.ups.edu.ec>
2023-07-19 17:35:21 -0500

    Add function registrar

--- frontend/src/app/sesion/sesion.component.ts
+++ frontend/src/app/sesion/sesion.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./sesion.component.css']
 })
 export class SesionComponent {
+  nombre: string = '';
+  apellido: string = '';
+  user: string = '';
+  correo: string = '';
+  password: string = '';
 
+  async envio(datos: any) {
+    const respuestaRaw = await fetch("172.18.0.2:8080", {
+      body: JSON.stringify(datos),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+    });
+    const jsonDecodificado = await respuestaRaw.json();
+    console.log(jsonDecodificado);
+  }
+  
+  registrar() {
+    let datos = {
+      nombre: this.nombre,
+      apellido: this.apellido,
+      user: this.user,
+      correo: this.correo,
+      password: this.password
+    };
+    this.envio(datos);
+  }
 }
