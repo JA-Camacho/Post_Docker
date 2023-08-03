3563d25e8c8d705262f718a448e7bca46c6908b9
Jose Camacho <jcamachor1@est.ups.edu.ec>
2023-08-03 15:53:25 -0500

    Add cors

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
-
+const cors = require('cors');
 //settings
 app.set('puerto', process.env.PORT || 8080);
 app.set('nombreApp', 'Aplicacion para Posts con docker');
 
 //Middlewares
 app.use(express.json());
+app.use(cors({origin: 'http://localhost:4200'}));
 
 //Routes
 app.use('/api/user', require('./routes/users.routes'));
