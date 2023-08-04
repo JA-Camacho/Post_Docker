6dba6c710c95043c5c5be40e4036617c99d6fc11
Jose Camacho <jcamachor1@est.ups.edu.ec>
2023-08-03 23:50:07 -0500

    Add id

--- frontend/src/app/models/user.ts
+++ frontend/src/app/models/user.ts
@@ -1,11 +1,13 @@
 export class User {
-    constructor(nombre = '', apellido = '', usuario = '', password = '', fecha_inicio_sesion = '') {
+    constructor(ID_usuario = '',nombre = '', apellido = '', usuario = '', password = '', fecha_inicio_sesion = '') {
+        this.ID_usuario = ID_usuario;
         this.nombre = nombre;
         this.apellido = apellido;
         this.usuario = usuario;
         this.password = password;
         this.fecha_inicio_sesion = fecha_inicio_sesion;
     }
+    ID_usuario : string;
     nombre: string;
     apellido: string;
     usuario: string;
