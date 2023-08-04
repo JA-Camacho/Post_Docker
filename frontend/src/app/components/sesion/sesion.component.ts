67d90cf652a1396ad606f2cadf3e06adac79af67
Jose Camacho <jcamachor1@est.ups.edu.ec>
2023-08-03 22:43:07 -0500

    Add method iniciar sesion

--- frontend/src/app/components/sesion/sesion.component.ts
+++ frontend/src/app/components/sesion/sesion.component.ts
@@ -1,5 +1,6 @@
 import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { User } from 'src/app/models/user';
 import { UserService } from 'src/app/services/user.service';
 
@@ -9,22 +10,39 @@ import { UserService } from 'src/app/services/user.service';
   styleUrls: ['./sesion.component.css']
 })
 export class SesionComponent {
-  fechaActual : string;
+  fechaActual: string;
   constructor(
     public userService: UserService,
-    private datePipe: DatePipe
-  ){
+    private datePipe: DatePipe,
+    private router: Router
+  ) {
     this.fechaActual = this.datePipe.transform(new Date(), 'yyyy-MM-dd') || '';
   }
   nombre = '';
   apellido = '';
   user = '';
   password = '';
-  
+
   registrar() {
     let user = new User(this.nombre, this.apellido, this.user, this.password, this.fechaActual);
-    this.userService.postUsers(user).subscribe((res)=>{
-      console.log(res);
+    this.userService.postUsers(user).subscribe((res) => {
+      localStorage.setItem('token', 'TokenEjemplo');
+      this.router.navigate(['/']);
     });
+
+  }
+
+  iniciarSesion() {
+    this.userService.getUser(this.user, this.password).subscribe(
+      (res) => {
+        if (res) {
+          localStorage.setItem('token', 'TokenEjemplo');
+          this.router.navigate(['/']);
+        }
+      },
+      (error) => {
+        return alert("Usuario No registrado");
+      }
+    );
   }
 }
