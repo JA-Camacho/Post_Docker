3c4a698a507abb466b7cd46b4bf602b09dc7efbc
Jose Camacho <jcamachor1@est.ups.edu.ec>
2023-08-03 23:50:53 -0500

    Post "new Post"

--- frontend/src/app/components/new-post/new-post.component.ts
+++ frontend/src/app/components/new-post/new-post.component.ts
@@ -1,4 +1,8 @@
+import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Post } from 'src/app/models/post';
+import { PostService } from 'src/app/services/post.service';
 
 @Component({
   selector: 'app-new-post',
@@ -6,9 +10,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./new-post.component.css']
 })
 export class NewPostComponent {
-titulo = '';
-contenido = '';
-subir() {
-  
-}
+  fechaActual: string;
+  constructor(
+    private datePipe: DatePipe,
+    private router: Router,
+    public postService: PostService
+  ){
+    this.fechaActual = this.datePipe.transform(new Date(), 'yyyy-MM-dd') || '';
+  }
+  titulo = '';
+  contenido = '';
+  subir() {
+    const ID = localStorage.getItem('ID') || '';
+    const autor = localStorage.getItem('autor') || '';
+    let post = new Post(ID,autor,this.titulo,this.contenido,this.fechaActual,this.fechaActual);
+    this.postService.postPosts(post).subscribe((res) => {
+      this.router.navigate(['/']);
+    })
+  }
 }
